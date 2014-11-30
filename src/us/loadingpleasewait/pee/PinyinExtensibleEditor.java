ace17ba8fa6c3b47469b8b37a5b1124b7fcba50f
Killin-A13 <amanuelbayu@gmail.com>
2014-11-30 00:12:24 -0600

    started on the GUI

--- src/us/loadingpleasewait/pee/PinyinExtensibleEditor.java
+++ src/us/loadingpleasewait/pee/PinyinExtensibleEditor.java
@@ -1,13 +1,33 @@
 package us.loadingpleasewait.pee;
 
+import java.awt.BorderLayout;
 import java.io.Serializable;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class PinyinExtensibleEditor implements Serializable{
 
 	private static final long serialVersionUID = -2539923325344870564L;
 
 	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		JTextField output = new JTextField("1");
+		JTextField input = new JTextField("2");
+		JPanel panel = new JPanel();
 		
+		frame.setTitle("Pinyin Extensible Editor");
+		frame.setSize(800, 500);
+		frame.setVisible(true);
+		frame.setResizable(true); 
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		panel.setLayout(new BorderLayout());
+		
+		panel.add(input);
+		output.setEditable(false);
+		panel.add(output);
+		frame.add(panel);
 	}
 
 }
