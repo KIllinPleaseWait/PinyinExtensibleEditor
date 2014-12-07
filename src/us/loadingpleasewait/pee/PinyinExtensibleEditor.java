203e07f87026fa38b57f60a2b59cdb3918deb1f1
LoadingPleaseWait <michael@murphey.org>
2014-12-06 22:38:36 -0600

    added methods to main class

--- src/us/loadingpleasewait/pee/PinyinExtensibleEditor.java
+++ src/us/loadingpleasewait/pee/PinyinExtensibleEditor.java
@@ -1,18 +1,50 @@
 package us.loadingpleasewait.pee;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.Serializable;
 
-public class PinyinExtensibleEditor implements Serializable {
+public class PinyinExtensibleEditor implements Serializable, ActionListener {
 
 	private static final long serialVersionUID = -2539923325344870564L;
+	
+	private GUI graphics;
 
 	/**
 	 * @param args program arguments (this is not used)
 	 */
 	public static void main(String[] args) {
+		new PinyinExtensibleEditor().start();
+	}
 
-		new GUI().initComponents();;
+	/**
+	 * Start the PinyinExtensibleEditor
+	 */
+	public void start() {
+		graphics = new GUI();
+		graphics.initComponents();
+	}
 
+	/**
+	 * @return the graphics
+	 */
+	protected GUI getGraphics() {
+		return graphics;
+	}
+
+	/**
+	 * @param graphics the graphics to set
+	 */
+	protected void setGraphics(GUI graphics) {
+		this.graphics = graphics;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		
 	}
 
 }
